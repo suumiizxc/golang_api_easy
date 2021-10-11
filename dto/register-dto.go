b0e5ec773ae1dd2101028a27f212a1ffa113df10
suumiizxc <ch.sumyadorj@gmail.com>
2021-10-11 18:22:49 +0800

    add register update

--- dto/register-dto.go
+++ dto/register-dto.go
@@ -1,7 +1,13 @@
 package dto
 
 type RegisterDTO struct {
-	Name     string `json:"name" form:"name" biding:"required" validate:"min:1"`
-	Email    string `json:"email" form:"email" binding:"required,email" validate:"email"`
-	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
+	Name              string `json:"name" form:"name" biding:"required" validate:"min:1"`
+	Email             string `json:"email" form:"email" binding:"required,email" validate:"email"`
+	Password          string `json:"password" form:"password" binding:"required" validate:"min:6"`
+	Registration      string `json:"registration" form:"registration" binding:"required" validate:"registration"`
+	PhoneNumber       string `json:"phone_number" form:"phone_number" binding:"required" validate:"phone_number"`
+	FirstName         string `json:"first_name" form:"first_name" binding:"required" validate:"first_name"`
+	LastName          string `json:"last_name" form:"last_name" binding:"required" validate:"last_name"`
+	BankName          string `json:"bank_name" form:"bank_name" binding:"required" validate:"bank_name"`
+	BankAccountNumber string `json:"bank_account_number" form:"bank_account_name" binding:"required" validate:"bank_account_number"`
 }
